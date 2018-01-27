8122c64ad60150e5a5aec8c2e4bbe12f5fd1b6fe
Matt <mrnewbigging@hotmail.com>
2018-01-27 12:19:16 +0000

    fixed whale falling thru floor

--- GGJ183D/Assets/_Scripts/BabyWhaleBehaviour.cs
+++ GGJ183D/Assets/_Scripts/BabyWhaleBehaviour.cs
@@ -5,30 +5,44 @@ using System.Collections;
 // Remains stationary until player within range and calls
 // Sends out calls at random intervals, and when player within range
 
-public class BabyWhaleBehaviour : MonoBehaviour {
+public class BabyWhaleBehaviour : MonoBehaviour
+{
 
 
     private void OnTriggerStay(Collider other)
     {
         // If the collision is with player
         if (other.tag == "Player")
-        { 
-            // This baby has been found; move to just behind player
-            transform.LookAt(other.transform);
-            transform.position += transform.forward * 5.0f * Time.deltaTime;
-            // Rotate to face the same way
+        {
+            // Parent to player when behind
+            if (Vector3.Distance(transform.position, other.transform.position) > 2)
+            {
+                Debug.Log("Travelling to parent");
+                // This baby has been found; move to just behind player
+                // Don't take Y axis into account
+                Vector3 target = new Vector3(other.transform.position.x + 5, transform.position.y, other.transform.position.z);
+                transform.LookAt(target);
+                transform.position += transform.forward * 5.0f * Time.deltaTime;
 
-            // Parent to player
+            }
+            // Otherwise, parent to play now within range
+            else
+            {
+                transform.SetParent(other.transform);
+                Debug.Log("Parented");
+            }
         }
     }
 
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
