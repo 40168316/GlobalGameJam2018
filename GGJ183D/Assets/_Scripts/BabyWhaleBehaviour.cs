9c73c3335162377d533e5f95efb5da6d0e85d342
Matt <mrnewbigging@hotmail.com>
2018-01-27 12:04:59 +0000

    baby following mamma whale

---
+++ GGJ183D/Assets/_Scripts/BabyWhaleBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Sandbox Mgr will spawn the babies somewhere on the map
+// Remains stationary until player within range and calls
+// Sends out calls at random intervals, and when player within range
+
+public class BabyWhaleBehaviour : MonoBehaviour {
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        // If the collision is with player
+        if (other.tag == "Player")
+        { 
+            // This baby has been found; move to just behind player
+            transform.LookAt(other.transform);
+            transform.position += transform.forward * 5.0f * Time.deltaTime;
+            // Rotate to face the same way
+
+            // Parent to player
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GGJ183D/Assets/_Scripts/MammaWhaleController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MammaWhaleController : MonoBehaviour {
+
+    private float moveSpeed = 5.0f;
+    private float turnSpeed = 100.0f;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Listen for input
+        PlayerInput();
+	}
+
+
+    private void PlayerInput()
+    {
+        // Listen for rotation via A/D, L/R keys
+        float rotation = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        // Rotate by above calculated amount
+        transform.Rotate(0, rotation, 0);
+        // Listen for space being held
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Move forwards
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+}
